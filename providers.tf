9e291ba8f26dd44ec0a2be2082cbe26b7fe85cd9
Kulsoom Mansoor <kulsoommansoor@Kulsooms-MacBook-Pro.local>
2021-12-09 15:45:32 -0800

    adding providers

---
+++ providers.tf
@@ -0,0 +1,11 @@
+terraform {
+    required_providers {
+      time = {
+          source = "hashicorp/time"
+          version = "0.6.0"
+      }
+      hashicups = {
+          source = "hashicorp/time"
+      }
+    }
+}
