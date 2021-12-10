4331ac88226379fea4451301f9476ead2814fea9
kulsoommansoor <kulsoomm786@gmail.com>
2021-12-09 16:16:46 -0800

    trying to trigger error message

--- providers.tf
+++ providers.tf
@@ -2,11 +2,10 @@ terraform {
     required_providers {
       time = {
           source = "hashicorp/time"
-          version = "0.7.0"
       }
       hashicups = {
           source = "example/edu/hashicups"
-          version = "0.3.2"
+          version = "0.3.3"
       }
     }
 }
