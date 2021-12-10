c9be93d7015cab130e5a573353a3b0086bfee6ce
kulsoommansoor <kulsoomm786@gmail.com>
2021-12-09 16:18:40 -0800

    trying trigger again

--- providers.tf
+++ providers.tf
@@ -2,6 +2,7 @@ terraform {
     required_providers {
       time = {
           source = "hashicorp/time"
+          version = "0.6.0"
       }
       hashicups = {
           source = "example/edu/hashicups"
